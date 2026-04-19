c0275da050fd6811719303a2f7f79b7f3fa843d3
Shivani Pushparajan <pushpars@oregonstate.edu>
2026-04-19 11:14:26 -0700

    Add function switchMode that switches user from the current mode to a new mode

--- scripts/modeTabs.js
+++ scripts/modeTabs.js
@@ -1,4 +1,27 @@
- /*************************************************************************
+ function switchMode(newMode) {
+    //Switch mode button
+    GlobalModeTabButtons[GlobalCurrentMode.get()].classList.remove("modetab-selected");
+    GlobalModeTabButtons[GlobalCurrentMode.get()].setAttribute("aria-selected",false);
+    GlobalModeTabButtons[newMode].classList.add("modetab-selected");
+    GlobalModeTabButtons[newMode].setAttribute("aria-selected",true);
+    //Switch tab panel
+    GlobalModeTabPanels[GlobalCurrentMode.get()].classList.add("hidden");
+    GlobalModeTabPanels[newMode].classList.remove("hidden");
+    GlobalCurrentMode.set(newMode); //Change mode
+    GlobalFocusedMode.set(newMode); //Change focused mode
+    if (GlobalHistoryLogging) {
+        const historyObj = {
+            page: GlobalModeNumbersToModes.get(newMode),
+            mode: newMode,
+            path: GlobalModeNumbersToRoutes.get(newMode)  
+        };   
+        history.pushState(historyObj, "", historyObj.path);
+        console.log("Console: In switchMode; pushing state: ", 
+            JSON.stringify(historyObj));
+    }
+}
+
+/*************************************************************************
  * @function keyDownModeTabFocused 
  * @Desc 
  * Handles valid keydown events when a mode tab button has the focus, 
