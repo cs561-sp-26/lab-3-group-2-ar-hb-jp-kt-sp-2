dc54fb54a7df5a6015e6fd1e8939cb9e54616159
Cassan <cassanbob00@gmail.com>
2026-04-19 12:01:03 -0700

    add keyDownMenuItemFocused

--- scripts/sideMenu.js
+++ scripts/sideMenu.js
@@ -44,3 +44,41 @@ for (let i = 0; i < GlobalMenuItems.length; ++i) {
       GlobalMenuBtn.click();
     });
 }
+
+/*************************************************************************
+* @function keyDownMenuItemFocused
+* @desc 
+* Handle keypress when menu is open and an item has focus. Per Table
+* 4.1 from the book, we handle the following key presses: tab, enter
+* escape, up arrow, down arrow, home, and end. 
+* are the arrow keys, space, and enter. All other keys are ignored.
+* @param key
+* The code of the key that was pressed.
+* @globals
+ * GlobalFocusedMenuItem is the index of the currently focused menu item
+ * GlobalMenuItems is an array of the HTML elements that are menu items   
+ * menuBtn is a reference to the menu button HTML element
+*************************************************************************/
+function keyDownMenuItemFocused(key) {  
+    if (key == "Enter") { //Activate focused menu item
+        document.activeElement.click();
+    } else if (key === "Tab") { //Close menu
+       GlobalMenuBtn.click();
+    } else if (key == "Escape") { //Close menu
+        GlobalMenuBtn.click();
+        GlobalMenuBtn.focus();
+    } else if (key === "ArrowUp") {  //Focus on next item
+        GlobalFocusedMenuItem.set((GlobalFocusedMenuItem.get() - 1 + GlobalMenuItems.length)
+          % GlobalMenuItems.length);
+        GlobalMenuItems[GlobalFocusedMenuItem.get()].focus();
+    } else if (key === "ArrowDown") {  //Focus on prev item                                                                                                                                             
+        GlobalFocusedMenuItem.set((GlobalFocusedMenuItem.get() + 1) % GlobalMenuItems.length);
+        GlobalMenuItems[GlobalFocusedMenuItem.get()].focus();
+    } else if (key === "Home") { //Focus on first item
+        GlobalFocusedMenuItem.set(0);
+        GlobalMenuItems[GlobalFocusedMenuItem.get()].focus();
+    } else if (key === "End") { //Focus on last item
+        GlobalFocusedMenuItem.set(GlobalMenuItems.length - 1);
+        GlobalMenuItems[GlobalFocusedMenuItem.get()].focus();
+    } 
+}
