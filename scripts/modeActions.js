774c02c526a3dae03f9bd4db55e193be0d052584
Nikomixo <kevint0218@outlook.com>
2026-04-19 11:07:51 -0700

    Implemented Floating Action Button CLICK handler

--- scripts/modeActions.js
+++ scripts/modeActions.js
@@ -18,7 +18,33 @@
  * @global dialogActionButtons: array of default ("OK") buttons for
  * each mode's dialog box
  *************************************************************************/
-
+for (let i = 0; i < GlobalModeActionButtons.length; ++i) {
+    GlobalModeActionButtons[i].addEventListener("click",function(e) {
+        //Hide tab panel
+        GlobalModeTabPanels[i].classList.add("hidden");
+        //Hide and disable all UI elements
+        GlobalMenuBtn.classList.add("disabled");
+        GlobalSearchBtn.classList.add("disabled");
+        GlobalProfileBtn.classList.add("disabled");
+        GlobalSkipLink.classList.add("hidden"); 
+        GlobalModeTabsContainer.classList.add("disabled");
+        //Show dialog box
+        GlobalModeActionDialogs[i].classList.remove("hidden");
+        //Set focus to dialog box's action button
+        GlobalDialogActionButtons[i].focus();
+        if (GlobalHistoryLogging) {
+            GlobalDialogClose = GlobalDialogCancelButtons[i];
+            const historyObj = {
+                page: "MODE_DIALOG",
+                mode: i,
+                path: GlobalModeDialogNumbersToRoutes.get(i)
+            };
+            history.pushState(historyObj, "", historyObj.path);
+            console.log("Console: In GlobalModeActionButtons click handler; pushing state: ", 
+                         JSON.stringify(historyObj));
+        }
+    });
+}
 /*************************************************************************
  * @function Dialog Box Action Button CLICK handler 
  * @Desc 
