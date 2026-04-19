ee457cf28b2f03eda2b655764f23dc69bb0bcc7b
Jackson Pope <jacksonpope2@gmail.com>
2026-04-19 11:00:47 -0700

    Created Window.js with Skeleton Fuctions

---
+++ window.js
@@ -0,0 +1,30 @@
+/********************************************************
+ * @function restoreState
+ * @descr
+ * Restores application state based on the state object 
+ * passed in.
+ * @param state: The state object to restore
+ * @global GlobalHistoryLogging: A boolean flag to control
+ * whether to log history state changes
+ * @global GlobalDialogClose: The dialog close button to
+ * click to close an open dialog box, if one exists
+ * @global GlobalModeActionButtons: Array of mode action
+ * buttons
+ * @global GlobalDialogCancelButtons: Array of dialog box
+ * cancel buttons
+ * @global GlobalModeTabButtons: Array of mode tab buttons
+ * @global GlobalModeTabPanels: Array of mode tab panels
+ * @global GlobalModeNamesMap: Maps mode names to integer
+ ********************************************************/
+function restoreState(state) {
+  
+}
+
+window.addEventListener('load', (event) => {
+
+});
+
+
+window.addEventListener('popstate', (event) => {
+
+});
