4115105e9d70009bb724cb5a6f5bd902bfe2ac89
Cassan <cassanbob00@gmail.com>
2026-04-19 11:57:31 -0700

    add menuBtn click handler

---
+++ scripts/sideMenu.js
@@ -0,0 +1,33 @@
+/*************************************************************************
+ * File: sideMenu.js
+ * These functions support interaction with the side menu.
+ ************************************************************************/
+
+/*************************************************************************
+ * @function menuBtn click handler
+ * @desc 
+ * When the user clicks the menuBtn, open or close the side menu 
+ * based on current menu state.
+ *************************************************************************/
+ document.getElementById("menuBtn")
+   .addEventListener("click", function (e) {
+    const sideMenu = document.getElementById("sideMenu");
+    const sideMenuIcon = document.getElementById("menuBtnIcon");
+    const sideMenuBtn = document.getElementById("menuBtn");
+    if (sideMenuIcon.classList.contains("fa-bars")) { //OPEN MENU
+        //Change menu icon
+        sideMenuIcon.classList.remove("fa-bars");
+        sideMenuIcon.classList.add("fa-times");
+        //Open menu
+        sideMenuBtn.setAttribute("aria-expanded","true"); 
+        sideMenu.classList.add("sidemenu-open");
+
+    } else { //CLOSE MENU
+        //Change menu icon
+        sideMenuIcon.classList.remove("fa-times");
+        sideMenuIcon.classList.add("fa-bars");
+        //Close menu
+        sideMenuBtn.setAttribute("aria-expanded","false");
+        sideMenu.classList.remove("sidemenu-open");
+    }
+});
